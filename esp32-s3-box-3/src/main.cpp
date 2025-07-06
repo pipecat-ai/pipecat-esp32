35be7897a9afbd23781dfb686e37de6c0e09a91a
Kwindla Hultman Kramer <kwindla@gmail.com>
2025-07-05 18:16:05 -0700

    Memory optimizations. Allocate minimal placeholder only for h264 (CONFIG_MAX_NALU_SIZE); Move LVGL graphics working memory out of internal memory to PSRAM.

--- esp32-s3-box-3/src/main.cpp
+++ esp32-s3-box-3/src/main.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 
+#include <inttypes.h>
 #include <esp_event.h>
 #include <esp_log.h>
 #include <peer.h>
@@ -8,15 +9,14 @@
 #include "nvs_flash.h"
 
 extern "C" void app_main(void) {
-  esp_err_t ret = nvs_flash_init();
-  if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
-      ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-    ESP_ERROR_CHECK(nvs_flash_erase());
-    ret = nvs_flash_init();
-  }
-  ESP_ERROR_CHECK(ret);
 
+  ESP_LOGI(LOG_TAG, "[APP] Startup..");
+  ESP_LOGI(LOG_TAG, "[APP] IDF version: %s", esp_get_idf_version());
+  heap_caps_print_heap_info(MALLOC_CAP_INTERNAL);
+
+  ESP_ERROR_CHECK(nvs_flash_init());
   ESP_ERROR_CHECK(esp_event_loop_create_default());
+
   pipecat_init_screen();
   peer_init();
   pipecat_init_audio_capture();
